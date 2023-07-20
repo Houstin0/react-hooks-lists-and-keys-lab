366bf9cca51b2b968a5b1873f7a21f4d4d8b68c6
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-07-20 20:50:49 +0300

    completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  let linksElement=links.map((link)=> <a key={link} href={`#${link}`}>{link}</a>)
+
+  return <nav>{/* display an <a> tag for each link here */
+  linksElement}</nav>;
 }
 
 export default NavBar;
+
